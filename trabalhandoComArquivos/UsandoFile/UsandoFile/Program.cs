e961e751d28e38c6dab06e602a6d0e8c93c324a6
Gabriel oliveira dos Reis <gabrie.o.reis@hotmail.com>
2020-03-19 12:25:19 -0300

    Trabalhando com a classe File

---
+++ trabalhandoComArquivos/UsandoFile/UsandoFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace UsandoFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\gabri\source\repos\Exercicios C#\trabalhandoComArquivos\AquivosCriados\file1.txt";
+            string targetPath = @"C:\Users\gabri\source\repos\Exercicios C#\trabalhandoComArquivos\AquivosCriados\file2.txt";
+
+            try
+            {
+                File.Copy(sourcePath, targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string s in lines)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
